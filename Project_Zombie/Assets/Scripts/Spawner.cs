f677eab01021e7d3df04355b2bacedadfb34a01e
Waltermelion <guilhermelpmeneses@gmail.com>
2022-02-26 18:48:53 +0000

    added zombie Spawner

---
+++ Project_Zombie/Assets/Scripts/Spawner.cs
@@ -0,0 +1,63 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private int waveNumber = 0;
+    private int enemySpawnAmmount = 0;
+    private int enemiesKilled = 0;
+    
+    public GameObject[] spawners;
+    public GameObject Zombie;
+
+    private void Start()
+    {
+        spawners = new GameObject[5];
+
+        for (int i = 0; i < spawners.Length; i++)
+        {
+            spawners[i] = transform.GetChild(i).gameObject;
+        }
+        
+        StartWave();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            SpawnEnemy();
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        int spawnerId = Random.Range(0, spawners.Length);
+        Instantiate(Zombie, spawners[spawnerId].transform.position, spawners[spawnerId].transform.rotation);
+    }
+
+    private void StartWave()
+    {
+        waveNumber = 1;
+        enemySpawnAmmount = 2;
+        enemiesKilled = 0;
+
+        for (int i = 0; i < enemySpawnAmmount; i++)
+        {
+            SpawnEnemy();
+        }
+    }
+
+    private void NextWave()
+    {
+        waveNumber++;
+        enemySpawnAmmount += 2;
+        enemiesKilled = 0;
+        
+        for (int i = 0; i < enemySpawnAmmount; i++)
+        {
+            SpawnEnemy();
+        }
+    }
+}
